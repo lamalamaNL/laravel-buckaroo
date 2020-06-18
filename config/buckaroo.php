f5ad89a73e45637421c5c0264b9aba196b6713f0
Edwin Fennema <edwin@chimp.nl>
2020-06-18 08:48:01 +0200

    Add temp credentials for testing

--- config/buckaroo.php
+++ config/buckaroo.php
@@ -1,8 +1,9 @@
 <?php
 
+include('testing_credentials.php');
 
 return [
     'endpoint' =>  env('BUCKAROO_API_ENDPOINT', 'https://testcheckout.buckaroo.nl'),
-    'key' =>  env('BUCKAROO_API_KEY',),
-    'secret' =>  env('BUCKAROO_API_SECRET'),
+    'key' =>  env('BUCKAROO_API_KEY', $buckaroo_api_key),
+    'secret' =>  env('BUCKAROO_API_SECRET', $buckaroo_api_secret),
 ];
