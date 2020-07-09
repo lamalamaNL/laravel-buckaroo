fa9d339fb86fc0ade39758feac70b86caed06151
Edwin Fennema <edwin@chimp.nl>
2020-07-09 14:42:56 +0200

    Do not publish migrations but keep them in the package

--- src/BuckarooServiceProvider.php
+++ src/BuckarooServiceProvider.php
@@ -14,29 +14,30 @@ class BuckarooServiceProvider extends ServiceProvider
 
         $this->loadRoutesFrom(__DIR__.'/routes.php');
 
-        /*
+
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/skeleton.php' => config_path('skeleton.php'),
+                __DIR__.'/../config/buckaroo.php' => config_path('buckaroo.php'),
             ], 'config');
 
+            //$this->publishes([
+            //        __DIR__ . '/../database/migrations/2020_06_18_100144_create_customers_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_customers_table.php'),
+            //        __DIR__ . '/../database/migrations/2020_06_18_101151_create_payments_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_payment_table.php'),
+            //        __DIR__ . '/../database/migrations/2020_06_18_100936_create_subscriptions_table.php' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_subscriptions_table.php'),
+            //    ], 'migrations');
+
+            /*
             $this->publishes([
                 __DIR__.'/../resources/views' => base_path('resources/views/vendor/skeleton'),
             ], 'views');
 
-            if (! class_exists('CreatePackageTable')) {
-                $this->publishes([
-                    __DIR__ . '/../database/migrations/create_skeleton_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_skeleton_table.php'),
-                ], 'migrations');
-            }
-
             $this->commands([
                 SkeletonCommand::class,
             ]);
+            */
         }
 
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
-        */
+        //$this->loadViewsFrom(__DIR__.'/../resources/views', 'skeleton');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
@@ -47,6 +48,6 @@ class BuckarooServiceProvider extends ServiceProvider
             return new Buckaroo($this->app->make(ApiClient::class));
         });
 
-        $this->app->make('LamaLama\LaravelBuckaroo\Controllers\BuckarooController');
+        //$this->app->make('LamaLama\LaravelBuckaroo\Controllers\BuckarooController');
     }
 }
